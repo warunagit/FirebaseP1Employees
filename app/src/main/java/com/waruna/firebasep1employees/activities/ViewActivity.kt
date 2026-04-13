a48839858c39f345d482b0d8f6a426c268a8a5f7
unknown <it.verbita@gmail.com>
2026-04-13 22:52:34 +0300

    v6 done

--- app/src/main/java/com/waruna/firebasep1employees/activities/ViewActivity.kt
+++ app/src/main/java/com/waruna/firebasep1employees/activities/ViewActivity.kt
@@ -2,14 +2,23 @@ package com.waruna.firebasep1employees.activities
 
 import android.os.Bundle
 import android.text.Editable
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
 import com.waruna.firebasep1employees.R
+import com.waruna.firebasep1employees.models.EmployeeModel
 
 class ViewActivity : AppCompatActivity() {
 
+
+    lateinit var receivedEmpId: String
+    lateinit var receivedName: String
+    lateinit var receivedAge: String
     lateinit var empId: String
     lateinit var etName: EditText
     lateinit var  etAge: EditText
@@ -24,7 +33,6 @@ class ViewActivity : AppCompatActivity() {
         setContentView(R.layout.activity_view)
 
         initView()
-        setValues()
 
         btnUpdate.setOnClickListener {
             openUpdateDialog(empId,intent.extras?.getString("name"))
@@ -37,13 +45,60 @@ class ViewActivity : AppCompatActivity() {
         etAge = findViewById(R.id.etEditAge)
         btnUpdate = findViewById(R.id.btnEditUpdate)
         btnDelete = findViewById(R.id.btnEditDelete)
+
+        receivedEmpId = intent.extras?.getString("empID").toString()
+        receivedName = intent.extras?.getString("name").toString()
+        receivedAge = intent.extras?.getString("age").toString()
+
+        setValues(receivedEmpId,receivedName,receivedAge)
     }
 
-    private fun setValues() {
-        empId = intent.extras?.getString("empID").toString()
-        etName.setText(intent.extras?.getString("name"))
-        etAge.setText(intent.extras?.getString("age"))
-    }2
+    private fun setValues(empID:String, name:String,age: String) {
+        empId = empID
+        etName.setText(""+name)
+        etAge.setText(""+age)
+    }
 
-    fun openUpdateDialog(empId: String?, empName: String?) {}
+    fun openUpdateDialog(empId: String?, empName: String?) {
+        Log.d("Log: ","Starting update")
+        val mDialog = AlertDialog.Builder(this)
+        val inflater = layoutInflater
+        val mDialogView = inflater.inflate(R.layout.activity_update,null)
+        mDialog.setView(mDialogView)
+
+        Log.d("Log: ","Inflate done")
+
+        val updateName = mDialogView.findViewById<EditText>(R.id.etUpdateName)
+        val updateAge = mDialogView.findViewById<EditText>(R.id.etUpdateAge)
+        val btnUpdateUpdate = mDialogView.findViewById<Button>(R.id.btnUpdateUpdate)
+
+        updateName.setText(receivedName.toString())
+        updateAge.setText(receivedAge.toString())
+
+        Log.d("Log: ","Setting data to alert done")
+
+        mDialog.setTitle("Updating the record of $receivedName")
+
+        val alertDialog = mDialog.create()
+        alertDialog.show()
+
+        btnUpdateUpdate.setOnClickListener {
+            updateEmployeeData(
+                receivedEmpId,
+                updateName.text.toString(),
+                updateAge.text.toString()
+            )
+            Toast.makeText(this,"Update data done", Toast.LENGTH_LONG)
+            setValues(receivedEmpId,updateName.text.toString(),updateAge.text.toString())
+
+            alertDialog.dismiss()
+        }
+    }
+
+    fun updateEmployeeData(empID: String, name: String, age: String) {
+        val dbRef = FirebaseDatabase.getInstance().getReference("employees").child(empID)
+        val employeeUpdateData = EmployeeModel(empID,name,age)
+        dbRef.setValue(employeeUpdateData)
+
+    }
 }
